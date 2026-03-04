d07e6daa62aa299997876ac81162466cef16ed02
anees028 <rehmananees028@gmail.com>
2026-03-04 11:48:56 +0100

    Main file for running updated.

--- coding-challenge/src/App.tsx
+++ coding-challenge/src/App.tsx
@@ -1,11 +1,12 @@
-import AnalysisDashboard from './AnalysisDashboard'
+import DashboardPage from './Page/DashboardPage';
 
 function App() {
   return (
-    <div className="min-h-screen bg-gray-50">
-      <AnalysisDashboard />
+    <div className="app-container">
+      {/* If you add React Router later, you would wrap DashboardPage in <Routes> here */}
+      <DashboardPage />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
