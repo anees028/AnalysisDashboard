618c91eac0346ed4a5f87eb26fd20a377fec3dfd
anees028 <rehmananees028@gmail.com>
2026-03-04 11:48:13 +0100

    Locales added for the UI and for multilangauge.

---
+++ coding-challenge/src/locales/translations.ts
@@ -0,0 +1,8 @@
+// Import translations from separate JSON files
+import enTranslations from './en.json'
+import deTranslations from './de.json'
+
+export const translations = {
+  'en': enTranslations,
+  'de': deTranslations
+}
